ca76a2f683d65962702585b61e38582e2a8cd819
kelsiec <kelsiec@maizeanalytics.com>
2018-07-26 22:03:27 -0700

    Add sorting and pagination to SsbTable

--- frontend/src/js/PoseTable.js
+++ frontend/src/js/PoseTable.js
@@ -12,7 +12,7 @@ var header = [
 ];
 const PoseTable = () => (
   <DataProvider endpoint="/poses/poses/"
-                render={data => <SsbTable header={header} data={data} />} />
+                render={data => <SsbTable header={header} data={data} orderBy='english_name' />} />
 );
 const wrapper = document.getElementById("pose-table-container");
 wrapper ? ReactDOM.render(<PoseTable />, wrapper) : null;
--- frontend/src/js/Table.js
+++ frontend/src/js/Table.js
@@ -1,36 +1,204 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import Checkbox from '@material-ui/core/Checkbox';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
+import TablePagination from '@material-ui/core/TablePagination';
 import TableRow from '@material-ui/core/TableRow';
+import TableSortLabel from '@material-ui/core/TableSortLabel';
+import Tooltip from '@material-ui/core/Tooltip';
 import Paper from '@material-ui/core/Paper';
 
-const SsbTable = (data) => (
-    <div className="column">
-        <Paper>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        {data.header.map(value => (
-                            <TableCell>{value}</TableCell>
-                        ))}
+
+function getSorting(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => (b[orderBy] < a[orderBy] ? -1 : 1)
+    : (a, b) => (a[orderBy] < b[orderBy] ? -1 : 1);
+}
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+  },
+  table: {
+    minWidth: 1020,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+});
+
+class SsbTable extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      header: props.header,
+      data: props.data,
+      order: props.order,
+      orderBy: props.orderBy,
+      selected: props.selected,
+      rowsPerPage: props.rowsPerPage,
+      page: props.page,
+    };
+  }
+
+  createSortHandler = property => event => {
+    this.handleRequestSort(event, property);
+  };
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property;
+    let order = 'desc';
+
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc';
+    }
+
+    this.setState({ order, orderBy });
+  };
+
+  handleSelectAllClick = (event, checked) => {
+    if (checked) {
+      this.setState(state => ({ selected: state.data.map(n => n.id) }));
+      return;
+    }
+    this.setState({ selected: [] });
+  };
+
+  handleClick = (event, id) => {
+    const { selected } = this.state;
+    const selectedIndex = selected.indexOf(id);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1),
+      );
+    }
+
+    this.setState({ selected: newSelected });
+  };
+
+  handleChangePage = (event, page) => {
+    this.setState({ page });
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value });
+  };
+
+  isSelected = id => this.state.selected.indexOf(id) !== -1;
+
+  render() {
+    const { classes } = this.props;
+    const { header, data, order, orderBy, selected, rowsPerPage, page } = this.state;
+    const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
+
+    const numSelected = selected.length;
+    const rowCount = data.length;
+
+    return (
+      <Paper className={classes.root}>
+        <div className={classes.tableWrapper}>
+          <Table className={classes.table} aria-labelledby="tableTitle">
+            <TableHead>
+              <TableRow>
+                <TableCell padding="checkbox">
+                  <Checkbox
+                    indeterminate={numSelected > 0 && numSelected < rowCount}
+                    checked={numSelected === rowCount}
+                    onChange={this.handleSelectAllClick}
+                  />
+                </TableCell>
+                {header.map((key, index) => {
+                  return <TableCell sortDirection={orderBy === index ? order : false}>
+                    <Tooltip title="Sort" placement={'bottom-start'} enterDelay={300} >
+                      <TableSortLabel
+                        active={orderBy === index}
+                        direction={order}
+                        onClick={this.createSortHandler(index)}
+                      >
+                        {key}
+                      </TableSortLabel>
+                    </Tooltip>
+                  </TableCell>;
+                })}
+              </TableRow>
+            </TableHead>
+            <TableBody>
+                {data
+                    .sort(getSorting(order, orderBy))
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map(row => {
+                        const isSelected = this.isSelected(row.id);
+                        return (
+                            <TableRow
+                                hover
+                                onClick={event => this.handleClick(event, row.id)}
+                                role="checkbox"
+                                aria-checked={isSelected}
+                                tabIndex={-1}
+                                key={row.id}
+                                selected={isSelected}
+                            >
+                                <TableCell padding="checkbox">
+                                    <Checkbox checked={isSelected}/>
+                                </TableCell>
+                                {Object.entries(row).map(cell => <TableCell>{cell[1]}</TableCell>)}
+                            </TableRow>
+                        )
+                    })
+                }
+                {emptyRows > 0 && (
+                    <TableRow style={{ height: 49 * emptyRows }}>
+                      <TableCell colSpan={header.length + 1} />
                     </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.data.map(el => (
-                        <TableRow key={el.id}>
-                            {Object.entries(el).map(el => <TableCell>{el[1]}</TableCell>)}
-                        </TableRow>
-                    ))}
-                </TableBody>
-            </Table>
-        </Paper>
-    </div>
-);
-Table.propTypes = {
-  data: PropTypes.array.isRequired
+                )}
+            </TableBody>
+          </Table>
+        </div>
+        <TablePagination
+          component="div"
+          count={data.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          backIconButtonProps={{
+            'aria-label': 'Previous Page',
+          }}
+          nextIconButtonProps={{
+            'aria-label': 'Next Page',
+          }}
+          onChangePage={this.handleChangePage}
+          onChangeRowsPerPage={this.handleChangeRowsPerPage}
+        />
+      </Paper>
+    )
+  }
+}
+
+SsbTable.defaultProps = {
+  order: 'asc',
+  selected: [],
+  page: 0,
+  rowsPerPage: 5,
 };
-export default SsbTable;
+
+SsbTable.propTypes = {
+    data: PropTypes.array.isRequired,
+    header: PropTypes.array.isRequired,
+    orderBy: PropTypes.string.isRequired,
+};
+
+export default withStyles(styles)(SsbTable);
