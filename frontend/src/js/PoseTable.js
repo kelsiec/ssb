80265be223eb7cb321c45907d41ae1de128da0f7
kelsiec <kelsiec@maizeanalytics.com>
2018-07-26 23:36:06 -0700

    fix most linting issues

--- frontend/src/js/PoseTable.js
+++ frontend/src/js/PoseTable.js
@@ -1,19 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import DataProvider from "./DataProvider";
-import SsbTable from "./Table";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import DataProvider from './DataProvider'
+import SsbTable from './Table'
 
-var header = [
-    "English Name",
-    "Sanskrit Name",
-    "Body Position",
-    "Spinal Position",
-    "Challenge Level"
-];
+const header = [
+  'English Name',
+  'Sanskrit Name',
+  'Body Position',
+  'Spinal Position',
+  'Challenge Level'
+]
 const PoseTable = () => (
-  <DataProvider endpoint="/poses/poses/"
-                render={data => <SsbTable header={header} data={data} orderBy='english_name' />} />
-);
-const wrapper = document.getElementById("pose-table-container");
-wrapper ? ReactDOM.render(<PoseTable />, wrapper) : null;
+  <DataProvider
+    endpoint='/poses/poses/'
+    render={data => <SsbTable header={header} data={data} orderBy='english_name' />}
+  />
+)
 
+const wrapper = document.getElementById('pose-table-container')
+wrapper ? ReactDOM.render(<PoseTable />, wrapper) : null
